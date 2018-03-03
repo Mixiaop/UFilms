30a8e90ccfc0e60417d300930f02919b4be0d303
Mixiaop <112949824@qq.com>
2018-03-03 12:36:04 +0800

    1

---
+++ src/UFilm.TestConsole/Beatricee/MovieInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFilm.TestConsole.Beatricee
+{
+    public class MovieInfo
+    {
+        public string No { get; set; }
+
+        public string PostDate { get; set; }
+
+        public string TimeLength { get; set; }
+
+        public string CreateCompany { get; set; }
+
+        public string PublishCompany { get; set; }
+
+        public string Categories { get; set; }
+
+
+    }
+}

aa488e6cc3932d7ba07560901453909e8374e340
Mixiaop <112949824@qq.com>
2018-03-03 13:14:40 +0800

    1

---
+++ src/UFilm.TestConsole/Beatricee/IResolver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UFilm.TestConsole.Beatricee
+{
+    public interface IResolver
+    {
+        MovieInfo Resolve(string txtFilePath);
+
+    }
+}
---
+++ src/UFilm.TestConsole/Beatricee/Resolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UFilm.TestConsole.Beatricee
+{
+    public class Resolver: IResolver
+    {
+        public MovieInfo Resolve(string txtFilePath) {
+            MovieInfo info = null;
+            if (File.Exists(txtFilePath))
+            {
+                info = new MovieInfo();
+                var html = File.ReadAllText(txtFilePath);
+
+
+            }
+
+            return info;
+        }
+    }
+}
